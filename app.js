49262abcc4f977a7ffbcf3ff2478c531d36086c3
MaZTeRMoN <maztermoon@gmail.com>
2023-01-30 22:34:18 +0700

    get start git
    เพิ่มไฟล์ เข้า git

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const chalk = require('chalk')
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+
+app.get("/", (req,res) =>{
+
+    res.send('Hello Diamond');
+
+})
+
+app.listen(port, ()=>{
+    console.log("Listening on port : " + chalk.blue(port));
+})
